f4bc4411da092856c719d8c0bb512be737e4b13f
Frank Wesdijk <frank.wesdijk@gmail.com>
2023-06-26 15:12:47 +0200

    FINISHED!

--- script/les13-1.js
+++ script/les13-1.js
@@ -7,7 +7,14 @@ $(function () {
 
 		slide: function (event, ui) {
 			handle.text(ui.value);
-			$("#prijsOutput").text("€ " + ui.value);
+
+			if (ui.value <= 19) {
+				$("#prijsOutput").text("€ " + (ui.value + 4.95));
+			} else if (ui.value >= 80) {
+				$("#prijsOutput").text("€ " + ui.value * 0.9);
+			} else {
+				$("#prijsOutput").text("€ " + ui.value);
+			}
 		},
 	});
 });
