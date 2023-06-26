8f4aae1529d5629afcf932a71457a966721b891b
Frank Wesdijk <frank.wesdijk@gmail.com>
2023-06-26 14:44:11 +0200

    basics les 1 set

---
+++ script/les13-1.js
@@ -0,0 +1,13 @@
+$(function () {
+	var handle = $("#custom-handle");
+	$("#slider").slider({
+		create: function () {
+			handle.text($(this).slider("value"));
+		},
+
+		slide: function (event, ui) {
+			handle.text(ui.value);
+			$("#prijsOutput").text("€ " + ui.value);
+		},
+	});
+});
