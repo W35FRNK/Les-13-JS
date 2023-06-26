4a56cf20c642b479d0894a1a1ccf056c45ae8dd0
Frank Wesdijk <frank.wesdijk@gmail.com>
2023-06-26 15:20:52 +0200

    nu echt klaar

--- script/les13-1.js
+++ script/les13-1.js
@@ -9,12 +9,17 @@ $(function () {
 			handle.text(ui.value);
 
 			if (ui.value <= 19) {
-				$("#prijsOutput").text("€ " + (ui.value + 4.95));
+				$("#prijsOutput").text("Uw kosten zijn € " + (ui.value * 4.95 + 5));
 			} else if (ui.value >= 80) {
-				$("#prijsOutput").text("€ " + ui.value * 0.9);
+				$("#prijsOutput").text("Uw kosten zijn € " + ui.value * 4, 455);
 			} else {
-				$("#prijsOutput").text("€ " + ui.value);
+				$("#prijsOutput").text("Uw kosten zijn € " + ui.value * 4.95);
 			}
+		},
+
+		stop: function (event, ui) {
+			handle.text(ui.value);
+			$("#waardeOutput").text("Het gekozen aantal is " + ui.value);
 		},
 	});
 });
